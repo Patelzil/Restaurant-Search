d8913b8efdea2db47063959ff4c8edf180a90d14
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-02-23 17:22:39 -0600

    new screen added to show more info.

---
+++ src/screens/RestaurantScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const RestaurantScreen = () => {
+    return <View>
+        <Text>Restaurant Screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default RestaurantScreen;
